d0d4d560830b485f0bfea8e71831a51bbb6c5d5f
Alexander Kristoffersen <akristoffersen@berkeley.edu>
2019-02-08 11:00:28 -0800

    starting animation, created while loop in main.

--- Fourier in Java/Main.java
+++ Fourier in Java/Main.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main (String[] args) {
+        T = args[0];
         Circle a; //test of basic squarewave
         Circle b;
         Circle c;
@@ -7,10 +8,18 @@ class Main {
         a = new Circle(4, Math.PI / 8, 0, null);
         b = new Circle(4 / 3, 3 * Math.PI / 8, 0, a);
         c = new Circle(4 / 5, 5 * Math.PI / 8, 0, b);
-        d = new Circle(4 / 7, 7 * Math.PI / 8, 0, c); 
-        for (int x = 0; x <= 20; x++) {
+        d = new Circle(4 / 7, 7 * Math.PI / 8, 0, c);
+
+        //Standard Drawing section
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(-10000, 10000);
+        StdDraw.clear();
+
+        Double time = 0.0;
+        while (time < T) {
             d.takestep();
             System.out.println(d.end);
+            time++;
         }
     }
 }
